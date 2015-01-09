d743d2e56b711e39be250dc73577bf7bafb96d74
Fabien Dibot <fabien.dibot@gmail.com>
2015-01-09 09:53:46 +0100

    Initial commit

---
+++ VMWare_Inventory.ps1
@@ -0,0 +1,21 @@
+﻿$Output = @()
+Get-Cluster 'Haute_Dispo_EVC' | Get-VM | Get-View | Where {-not $_.Config.Template} | Select *, @{N="Disks";E={@($_.Guest.Disk.Length)}} | Sort-Object -Descending Disks | % {
+    $VM = $_
+    $prop = New-Object System.Collections.Specialized.OrderedDictionary
+    $prop.add('Name',$VM.name)
+    $prop.add('CPUSocket',$vm.config.hardware.NumCPU)
+    $prop.add('CorePerSocket',$vm.config.hardware.NumCoresPerSocket)
+    $Disk = 0
+    $TotalSize = 0
+    $VM.Guest.Disk | % {
+        $prop.add("Disk$($Disk) Letter/Path",$_.DiskPath)
+        $prop.add("Disk$($Disk) Capacity (GB)",[math]::Round($_.Capacity/ 1GB))
+        $prop.add("Disk$($Disk) FreeSpace (GB)",[math]::Round($_.FreeSpace / 1GB))
+        $TotalSize = $TotalSize + [math]::Round($_.Capacity/ 1GB)
+        $Disk++
+    }
+    $prop.add('TotalSize',$TotalSize)
+    $Output += [PSCustomObject]$prop
+}
+
+$Output
